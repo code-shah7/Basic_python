bee3c50daff635ba6e5cf11714721e170cb5826b
code-shah7 <imshershah77@gmail.com>
2024-11-15 21:38:20 +0530

    new program

--- Shradha Khapra/FirstProgram.py
+++ Shradha Khapra/FirstProgram.py
@@ -1,12 +1,41 @@
-print("I'm Aditya Shukla", "AKA","SHERSHAH",)
-print(7781)
-print("Date")
-print("01-11-2024")
+# print("I'm Aditya Shukla", "AKA","SHERSHAH",)
+# print(7781)
+# print("Date")
+# print("01-11-2024")
  
-name = "Aditya "
-age = 19
-mark = 100.00
+# name = "Aditya "
+# age = 19
+# mark = 100.00
 
-print(type (name))
-print(type(age))
-print(type(mark))
+# print(type (name))
+# print(type(age))
+# print(type(mark))
+
+
+
+# # #first program
+# # #sum of to no.
+# a=int(input("Enter first number:"))
+# b=int(input("Enter Second number:"))
+# c=a+b
+# print("sum of a and b ", c)
+
+
+# #WAP to input side of a square & print its area.
+# side = int(input("Enter the side of Square :"))
+# Area= side*side
+# print("Area of Square", Area)
+
+# #WAP to input 2 floating point numbers & print their average.
+# a= float(input("Enter your First no.:"))
+# b= float(input("Enter your Second no."))
+# c= a+b
+# avg=c/2
+# print("Average of first and second no.", avg)
+
+# # WAP to input 2 int numbers, a and b.
+# # Print True if a is greater than or equal to b. if not print False
+# a=int(input("enter first value:"))
+# b=int(input("enter second value:"))
+
+# print(bool(a >= b))
