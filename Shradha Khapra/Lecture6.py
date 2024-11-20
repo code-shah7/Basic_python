33e978e14e946a0397c3e681108ee549235af6d0
code-shah7 <imshershah77@gmail.com>
2024-11-20 22:19:17 +0530

    updated

--- Shradha Khapra/Lecture6.py
+++ Shradha Khapra/Lecture6.py
@@ -103,3 +103,10 @@ def natural(n):
 natural(6)
 
 #write a recursive function to print all elements in a list.
+
+def print_list(list, idx=0):
+    if(idx == len(list)):
+          return
+    print(list[idx])
+    print_list(list, idx+1)
+fruits = ["mango",'apple', 'bannana',]
