32f15a473cad3191d1447ae415889720941a07d6
code-shah7 <imshershah77@gmail.com>
2024-11-22 21:55:51 +0530

    add new folder and dlt let us py

---
+++ Shradha Khapra/Lecture8.py
@@ -0,0 +1,10 @@
+#OOP
+#Class & Object 
+#class is a blueprint for creating objects.
+#creating class
+class student:
+    name = "aman"
+
+#creating object (instance)
+s1 = student() 
+print(s1.name)
